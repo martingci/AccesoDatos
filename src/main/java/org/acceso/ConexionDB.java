398aadd4249ecbb80d750df174c17b49e04d4dc8
martingci <m.carrasco29@ufromail.cl>
2025-07-11 19:23:33 -0400

    Se añade clase de conexión por separado.

---
+++ src/main/java/org/acceso/ConexionDB.java
@@ -0,0 +1,20 @@
+package org.acceso;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    // parámetros para conexión a la base de datos
+    private static final String URL = "jdbc:postgresql://localhost:5432/biblioteca";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "ICC505";
+
+    // Conecta con la base de datos.
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver"); // Cargar el driver de PostgreSQL
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+}
