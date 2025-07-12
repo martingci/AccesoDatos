f57a884a4ae3fac8e3271fda44b8697246a5cdeb
Sabrina <s.lopez15@ufromail.cl>
2025-07-11 21:13:09 -0400

    layout operaciones

--- src/main/java/org/acceso/Operaciones.java
+++ src/main/java/org/acceso/Operaciones.java
@@ -3,4 +3,48 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Operaciones {
+    public static void insertarValores(String tabla){
+        Connection conexion = ConexionDB.conectar();
+        if (conexion == null) {
+            System.out.println("No se pudo establecer la conexión.");
+            return;
+        }
+        conexion.setAutoCommit(false);
+    }
+
+    public static void registrarPrestamo(){
+        Connection conexion = ConexionDB.conectar();
+        if (conexion == null) {
+            System.out.println("No se pudo establecer la conexión.");
+            return;
+        }
+        conexion.setAutoCommit(false);
+    }
+
+    public static void Actualizar(String tabla){
+        Connection conexion = ConexionDB.conectar();
+        if (conexion == null) {
+            System.out.println("No se pudo establecer la conexión.");
+            return;
+        }
+        conexion.setAutoCommit(false);
+    }
+
+    public static void consultarValores(String tabla){
+        Connection conexion = ConexionDB.conectar();
+        if (conexion == null) {
+            System.out.println("No se pudo establecer la conexión.");
+            return;
+        }
+        conexion.setAutoCommit(false);
+    }
+
+    public static void eliminarValores(String tabla){
+        Connection conexion = ConexionDB.conectar();
+        if (conexion == null) {
+            System.out.println("No se pudo establecer la conexión.");
+            return;
+        }
+        conexion.setAutoCommit(false);
+    }
 }
